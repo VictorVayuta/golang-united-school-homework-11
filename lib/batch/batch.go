dc5727b9ea327c91ed96391c2b992262edebdbb1
Victor Vayuta <eoll073@gmail.com>
2022-07-06 23:12:02 +0300

    first commit

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,7 @@
 package batch
 
 import (
+	"sync"
 	"time"
 )
 
@@ -14,5 +15,28 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	res = make([]user, n)
+
+	const paralel = 10
+
+	var wg sync.WaitGroup
+
+	sem := make(chan struct{}, paralel)
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func() {
+			sem <- struct{}{}
+			getOne(pool)
+			<-sem
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+	return res
 }
+
+//func work() {
+//	time.Sleep(1 * time.Second)
+//	fmt.Println("Work is done")
+//}
